52c51a0a9cebac9e540246ea42cea8ec5bf6f6f2
Panagiotis Stenos <panstenos@gmail.com>
2022-11-13 11:32:10 +0000

    Exercise 1.1
    Initialise serial function
    Initialise main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+// CONFIG1L
+#pragma config FEXTOSC = HS     // External Oscillator mode Selection bits (HS (crystal oscillator) above 8 MHz; PFM set to high power)
+#pragma config RSTOSC = EXTOSC_4PLL// Power-up default value for COSC bits (EXTOSC with 4x PLL, with EXTOSC operating per FEXTOSC bits)
+
+// CONFIG3L
+#pragma config WDTE = OFF        // WDT operating mode (WDT enabled regardless of sleep)
+
+#include <xc.h>
+//#include "LEDarray.h"
+#include "interrupts.h"
+#include "timers.h"
+#include "ADC.h"
+#include "LCD.h"
+//#include "light_manager.h"
+#include "serial.h"
+
+#define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz
+
+void main(void) {
+    LCD_Init();
+    initUSART4(); 
+
+    while (1) {
+        LCD_sendbyte(getCharSerial4(),1);
+    }
+}
--- serial.c
+++ serial.c
@@ -2,9 +2,17 @@
 #include "serial.h"
 
 void initUSART4(void) {
-
+    RC0PPS = 0x12; // Map EUSART4 TX to RC0
+    RX4PPS = 0x11; // RX is RC1
 	//code to set up USART4 for Reception and Transmission =
-	//see readme for detials
+    BAUD4CONbits.BRG16 = 0; 	//set baud rate scaling
+    TX4STAbits.BRGH = 0; 		//high baud rate select bit
+    SP4BRGL = 103; 			//set baud rate to 103 = 9600bps
+    SP4BRGH = 0;			//not used
+
+    RC4STAbits.CREN = 1; 		//enable continuos reception
+    TX4STAbits.TXEN = 1; 		//enable transmitter
+    RC4STAbits.SPEN = 1; 		//enable serial port	
 }
 
 //function to wait for a byte to arrive on serial port and read it once it does 
